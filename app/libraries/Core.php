24b452c7952923992ba80bb0f5bdabb4f6dc563d
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2018-07-12 14:24:05 +0530

    Instantiate current controller in Core.php, create Pages controller for code testing

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -9,4 +9,33 @@ class Core {
   protected $currentController = 'Pages';
   protected $currentMethod = 'index';
   protected $params = [];
+
+  public function __construct() {
+    $url = $this->getUrl();
+
+    // Checking if controller exists.
+    if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+      // Assigning value to currentController.
+      $this->currentController = ucwords($url[0]);
+
+      unset($url[0]);
+    }
+
+    require_once '../app/controllers/' . $this->currentController . '.php';
+    // Instantiating current controller.
+    $this->currentController = new $this->currentController;
+  }
+
+  public function getUrl() {
+    // Checking if url key exists in $_GET.
+    if(isset($_GET['url'])) {
+      // Triming right most /, if any.
+      $url = rtrim($_GET['url'], '/');
+      // Sanitizing the string as a URL.
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      $url = explode('/', $url);
+
+      return $url;
+    }
+  }
 }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once '../app/bootstrap.php';
+
+
+// init core 
+$core = new Core;
