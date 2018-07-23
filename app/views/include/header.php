361f533578682980f351a18f985d3ea2098ec5f4
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2018-07-23 23:31:23 +0530

    Included header and footer into the views files

---
+++ app/views/include/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo SITENAME; ?></title>
+  <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+</head>
+<body>
--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,1 +1,3 @@
-ABout
+<?php require_once APPROOT . 'views/include/header.php' ?>
+<h1><?php echo $data['title']; ?></h1>
+<?php require_once APPROOT . 'views/include/footer.php' ?>
