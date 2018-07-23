fd0f08f3e6295ef562a5f7c189937cdeecbd3557
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2018-07-23 17:11:17 +0530

    added views

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,16 +1,18 @@
 <?php
 
-class Pages extends Controller{
+class Pages extends Controller {
   public function __construct() {
     echo 'Pages loaded';
   }
 
-  public function index()
-  {
-    
+  public function index() {
+    $data = [
+      'title' => 'VA-MVC',
+    ];
+    $this->view('pages/index', $data);
   }
-  public function about()
-  {
-    
+
+  public function about() {
+    $this->view('pages/about');
   }
 }
